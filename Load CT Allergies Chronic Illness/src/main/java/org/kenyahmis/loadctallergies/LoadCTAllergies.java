6be62b94a044cae22a50762accc6d53da8adcf46
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-11-15 15:05:11 +0300

    Use left anti join to find unmatched allergy rows

--- Load CT Allergies Chronic Illness/src/main/java/org/kenyahmis/loadctallergies/LoadCTAllergies.java
+++ Load CT Allergies Chronic Illness/src/main/java/org/kenyahmis/loadctallergies/LoadCTAllergies.java
@@ -6,13 +6,11 @@ import org.apache.spark.sql.*;
 import org.apache.spark.storage.StorageLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.Arrays;
 
-import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.*;
 
 public class LoadCTAllergies {
     private static final Logger logger = LoggerFactory.getLogger(LoadCTAllergies.class);
@@ -24,7 +22,6 @@ public class LoadCTAllergies {
                 .config(conf)
                 .getOrCreate();
         RuntimeConfig rtConfig = session.conf();
-        final int targetPartitions = 10;
 
         final String queryFileName = "LoadCTAllergies.sql";
         String query;
@@ -51,8 +48,7 @@ public class LoadCTAllergies {
                 .option("numpartitions", rtConfig.get("spark.source.numpartitions"))
                 .load();
 
-        sourceDataFrame.persist(StorageLevel.MEMORY_AND_DISK());
-//        sourceDataFrame.printSchema();
+        sourceDataFrame.persist(StorageLevel.DISK_ONLY());
         logger.info("Loading target ct allergies data frame");
         Dataset<Row> targetDataFrame = session.read()
                 .format("jdbc")
@@ -63,49 +59,29 @@ public class LoadCTAllergies {
                 .option("dbtable", rtConfig.get("spark.sink.dbtable"))
                 .option("numpartitions", rtConfig.get("spark.sink.numpartitions"))
                 .load();
-        targetDataFrame.persist(StorageLevel.MEMORY_AND_DISK());
+        targetDataFrame.persist(StorageLevel.DISK_ONLY());
 
-        // source comparison data frame
-        Dataset<Row> sourceComparisonDf = sourceDataFrame.select(col("PatientID"), col("PatientPK"),
-                col("SiteCode"), col("VisitID"), col("VisitDate"));
+        sourceDataFrame.createOrReplaceTempView("source_allergies");
+        targetDataFrame.createOrReplaceTempView("target_allergies");
 
-        // target comparison data frame
-        Dataset<Row> targetComparisonDf = targetDataFrame.select(col("PatientID"), col("PatientPK"),
-                col("SiteCode"), col("VisitID"), col("VisitDate"));
+        Dataset<Row> unmatchedFromJoinDf = session.sql("SELECT t.* FROM target_allergies t LEFT ANTI JOIN source_allergies s ON s.SiteCode <=> t.SiteCode AND" +
+                " s.PatientPK <=> t.PatientPK AND s.VisitID <=> t.VisitID");
 
-        // Records in target data frame and not in source data frame
-        Dataset<Row> unmatchedFacilities = targetComparisonDf.except(sourceComparisonDf)
-                .withColumnRenamed("PatientID", "UN_PatientID")
-                .withColumnRenamed("PatientPK", "UN_PatientPK")
-                .withColumnRenamed("SiteCode", "UN_SiteCode")
-                .withColumnRenamed("VisitID", "UN_VisitID")
-                .withColumnRenamed("VisitDate", "UN_VisitDate");
-
-        Dataset<Row> finalUnmatchedDf = unmatchedFacilities.join(targetDataFrame,
-                targetComparisonDf.col("PatientID").equalTo(unmatchedFacilities.col("UN_PatientID")).and(
-                        targetComparisonDf.col("PatientPK").equalTo(unmatchedFacilities.col("UN_PatientPK"))
-                ).and(
-                        targetComparisonDf.col("SiteCode").equalTo(unmatchedFacilities.col("UN_SiteCode"))
-                ).and(
-                        targetComparisonDf.col("VisitID").equalTo(unmatchedFacilities.col("UN_VisitID"))
-                ).and(
-                        targetComparisonDf.col("VisitDate").equalTo(unmatchedFacilities.col("UN_VisitDate"))
-                ), "inner");
-
-        finalUnmatchedDf.createOrReplaceTempView("final_unmatched");
-        sourceDataFrame.createOrReplaceTempView("source_dataframe");
-
-        String sourceColumns = Arrays.toString(sourceDataFrame.columns());
-        logger.info("Source columns: " + sourceColumns);
+        long unmatchedVisitCount = unmatchedFromJoinDf.count();
+        logger.info("Unmatched count after target join is: " + unmatchedVisitCount);
+        unmatchedFromJoinDf.createOrReplaceTempView("final_unmatched");
 
         Dataset<Row> mergeDf1 = session.sql("select PatientID, PatientPK, SiteCode, FacilityName, VisitID, VisitDate, Emr, Project, ChronicIllness, ChronicOnsetDate, knownAllergies, AllergyCausativeAgent, AllergicReaction, AllergySeverity, AllergyOnsetDate, Skin, Eyes, ENT, Chest, CVS, Abdomen, CNS, Genitourinary, DateImported, CKV from final_unmatched");
-        Dataset<Row> mergeDf2 = session.sql("select PatientID, PatientPK, SiteCode, FacilityName, VisitID, VisitDate, Emr, Project, ChronicIllness, ChronicOnsetDate, knownAllergies, AllergyCausativeAgent, AllergicReaction, AllergySeverity, AllergyOnsetDate, Skin, Eyes, ENT, Chest, CVS, Abdomen, CNS, Genitourinary, DateImported, CKV from source_dataframe");
+        Dataset<Row> mergeDf2 = session.sql("select PatientID, PatientPK, SiteCode, FacilityName, VisitID, VisitDate, Emr, Project, ChronicIllness, ChronicOnsetDate, knownAllergies, AllergyCausativeAgent, AllergicReaction, AllergySeverity, AllergyOnsetDate, Skin, Eyes, ENT, Chest, CVS, Abdomen, CNS, Genitourinary, DateImported, CKV from source_allergies");
+
+        mergeDf2.printSchema();
+        mergeDf1.printSchema();
 
         // Union all records together
-        Dataset<Row> dfMergeFinal = mergeDf1.unionAll(mergeDf2);
-        dfMergeFinal.printSchema();
+        Dataset<Row> dfMergeFinal = mergeDf1.union(mergeDf2);
+        long mergedFinalCount = dfMergeFinal.count();
+        logger.info("Merged final count: " + mergedFinalCount);
         dfMergeFinal
-                .repartition(targetPartitions)
                 .write()
                 .format("jdbc")
                 .option("url", rtConfig.get("spark.sink.url"))
