d48da487a274a33f01a1c172df790f15853d7c06
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-11-16 20:12:18 +0300

    Add patient status cleaning script

--- Load Patient Pharmacy Job/src/main/resources/LoadSourcePatientPharmacy.sql
+++ Load Patient Pharmacy Job/src/main/resources/LoadSourcePatientPharmacy.sql
@@ -1,28 +1,34 @@
 SELECT
-    P.[PatientCccNumber] AS PatientID, P.[PatientPID] AS PatientPK,F.[Name] AS FacilityName, F.Code AS SiteCode,PP.[VisitID] VisitID,PP.[Drug] Drug
-     ,PP.[DispenseDate] DispenseDate,PP.[Duration] Duration,PP.[ExpectedReturn] ExpectedReturn,PP.[TreatmentType] TreatmentType
-     ,PP.[PeriodTaken] PeriodTaken,PP.[ProphylaxisType] ProphylaxisType,P.[Emr] Emr
+    P.[PatientCccNumber] AS PatientID,
+    P.[PatientPID] AS PatientPK,
+    F.Name AS FacilityName,
+    F.Code AS SiteCode
+     ,PS.[ExitDescription] ExitDescription
+     ,PS.[ExitDate] ExitDate
+     ,PS.[ExitReason] ExitReason
+     ,P.[Emr] Emr
      ,CASE P.[Project]
           WHEN 'I-TECH' THEN 'Kenya HMIS II'
           WHEN 'HMIS' THEN 'Kenya HMIS II'
           ELSE P.[Project]
     END AS [Project]
--- 					  ,PP.[Voided] Voided
--- 					  ,PP.[Processed] Processed
--- 					  ,PP.[Provider] [Provider]
-					  ,PP.[RegimenLine] RegimenLine
--- 					  ,PP.[Created] Created
-					  ,PP.RegimenChangedSwitched RegimenChangedSwitched
-					  ,PP.RegimenChangeSwitchReason RegimenChangeSwitchReason
-					  ,PP.StopRegimenReason StopRegimenReason
-					  ,PP.StopRegimenDate StopRegimenDate
-					  ,LTRIM(RTRIM(STR(F.Code)))+'-'+LTRIM(RTRIM(P.[PatientCccNumber])) +'-'+LTRIM(RTRIM(STR(P.[PatientPID]))) AS CKV
--- 					  0 AS IsRegimenFlag,
--- 					  0 AS KnockOutDrug
-					  ,P.ID as PatientUnique_ID
-					  ,PP.ID as PatientPharmacyUnique_ID
-FROM [DWAPICentral].[dbo].[PatientExtract] P
-    INNER JOIN [DWAPICentral].[dbo].[PatientArtExtract] PA ON PA.[PatientId]= P.ID
-    INNER JOIN [DWAPICentral].[dbo].[PatientPharmacyExtract] PP ON PP.[PatientId]= P.ID AND PP.Voided=0
-    INNER JOIN [DWAPICentral].[dbo].[Facility] F ON P.[FacilityId] = F.Id AND F.Voided=0
+
+						  ,PS.[Voided] Voided
+						  ,PS.[Processed] Processed
+						  ,PS.[Created] Created
+						  ,LTRIM(RTRIM(STR(F.Code)))+'-'+LTRIM(RTRIM(P.[PatientCccNumber])) +'-'+LTRIM(RTRIM(STR(P.[PatientPID]))) AS PKV,
+						NULL AS PatientUID,
+						[ReasonForDeath],
+						[SpecificDeathReason],
+						Cast([DeathDate] as Date)[DeathDate],
+						EffectiveDiscontinuationDate,
+						PS.TOVerified TOVerified,
+						PS.TOVerifiedDate TOVerifiedDate,
+						PS.ReEnrollmentDate ReEnrollmentDate,
+						P.ID as PatientUnique_ID,
+						PS.ID as PatientStatusUnique_ID
+FROM [DWAPICentral].[dbo].[PatientExtract] P WITH (NoLock)
+    INNER JOIN [DWAPICentral].[dbo].[PatientStatusExtract]PS WITH (NoLock)  ON PS.[PatientId]= P.ID AND PS.Voided=0
+    INNER JOIN [DWAPICentral].[dbo].[Facility] F (NoLock)  ON P.[FacilityId] = F.Id AND F.Voided=0
+    ---INNER JOIN FacilityManifest_MaxDateRecieved(NoLock) a ON F.Code = a.SiteCode and a.[End] is not null and a.[Session] is not null
 WHERE p.gender!='Unknown'
