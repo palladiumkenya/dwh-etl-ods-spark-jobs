7d1cf286393b4e46c78390eba5c2ddc2c5afdff5
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-11-18 02:01:08 +0300

    Use left anti join to find unmatched patient records

--- Load CT Patients Job/src/main/java/org/kenyahmis/loadctpatients/LoadCTPatients.java
+++ Load CT Patients Job/src/main/java/org/kenyahmis/loadctpatients/LoadCTPatients.java
@@ -11,8 +11,10 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-
-import static org.apache.spark.sql.functions.lit;
+import java.sql.Date;
+import java.time.LocalDate;
+import static org.apache.spark.sql.functions.*;
+import static org.apache.spark.sql.functions.col;
 
 public class LoadCTPatients {
     private static final Logger logger = LoggerFactory.getLogger(LoadCTPatients.class);
@@ -26,9 +28,7 @@ public class LoadCTPatients {
         RuntimeConfig rtConfig = session.conf();
 
         final String sourceQueryFileName = "LoadSourceCTPatients.sql";
-        final String targetQueryFileName = "LoadTargetCTPatients.sql";
         String sourceVisitsQuery;
-        String targetQuery;
         InputStream inputStream = LoadCTPatients.class.getClassLoader().getResourceAsStream(sourceQueryFileName);
         if (inputStream == null) {
             logger.error(sourceQueryFileName + " not found");
@@ -41,17 +41,6 @@ public class LoadCTPatients {
             return;
         }
 
-        InputStream targetQueryInputStream = LoadCTPatients.class.getClassLoader().getResourceAsStream(targetQueryFileName);
-        if (targetQueryInputStream == null) {
-            logger.error(targetQueryFileName + " not found");
-            return;
-        }
-        try {
-            targetQuery = IOUtils.toString(targetQueryInputStream, Charset.defaultCharset());
-        } catch (IOException e) {
-            logger.error("Failed to load target CT patient from file", e);
-            return;
-        }
         logger.info("Loading source CT patients");
         Dataset<Row> sourceDf = session.read()
                 .format("jdbc")
@@ -62,13 +51,40 @@ public class LoadCTPatients {
                 .option("dbtable", "(" + sourceVisitsQuery + ") pv")
                 .option("numpartitions", rtConfig.get("spark.source.numpartitions"))
                 .load();
+
         sourceDf = sourceDf
-                .withColumn("RegistrationYear", lit(null).cast(DataTypes.IntegerType))
-                .withColumn("MPIPKV", lit(null));
+                .withColumn("DOB", when(col("DOB").lt(lit(Date.valueOf(LocalDate.of(1910, 1, 1))))
+                        .or(col("DOB").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("DOB")))
+                .withColumn("RegistrationDate", when(col("RegistrationDate").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("RegistrationDate").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("RegistrationDate")))
+                .withColumn("RegistrationAtCCC", when(col("RegistrationAtCCC").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("RegistrationAtCCC").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("RegistrationAtCCC")))
+                .withColumn("RegistrationAtPMTCT", when(col("RegistrationAtPMTCT").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("RegistrationAtPMTCT").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("RegistrationAtPMTCT")))
+                .withColumn("RegistrationAtTBClinic", when(col("RegistrationAtTBClinic").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("RegistrationAtTBClinic").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("RegistrationAtTBClinic")))
+                .withColumn("PreviousARTStartDate", when(col("PreviousARTStartDate").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("PreviousARTStartDate").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("PreviousARTStartDate")))
+                .withColumn("PreviousARTStartDate", when(col("PreviousARTStartDate").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("PreviousARTStartDate").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("PreviousARTStartDate")))
+                .withColumn("LastVisit", when(col("LastVisit").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("LastVisit").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("LastVisit")))
+                .withColumn("DateConfirmedHIVPositive", when(col("DateConfirmedHIVPositive").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("DateConfirmedHIVPositive").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("DateConfirmedHIVPositive")))
+                .withColumn("TransferInDate", when(col("TransferInDate").lt(lit(Date.valueOf(LocalDate.of(1980, 1, 1))))
+                        .or(col("TransferInDate").gt(lit(Date.valueOf(LocalDate.now())))), lit(Date.valueOf(LocalDate.of(1900, 1, 1))))
+                        .otherwise(col("TransferInDate")));
 
-        sourceDf.printSchema();
-
-        sourceDf.persist(StorageLevel.MEMORY_AND_DISK());
+        sourceDf.persist(StorageLevel.DISK_ONLY());
 
         logger.info("Loading target CT patients");
         Dataset<Row> targetDf = session.read()
@@ -77,22 +93,49 @@ public class LoadCTPatients {
                 .option("driver", rtConfig.get("spark.sink.driver"))
                 .option("user", rtConfig.get("spark.sink.user"))
                 .option("password", rtConfig.get("spark.sink.password"))
-                .option("dbtable", "(" + targetQuery + ") ps")
+                .option("dbtable", rtConfig.get("spark.sink.dbtable"))
                 .load();
 
-        targetDf.persist(StorageLevel.MEMORY_AND_DISK());
-        targetDf.printSchema();
+        targetDf.persist(StorageLevel.DISK_ONLY());
 
-        // Find rows in target table unmatched in source table
-        Dataset<Row> unmatchedDf = targetDf.except(sourceDf);
+        sourceDf.createOrReplaceTempView("source_patients");
+        targetDf.createOrReplaceTempView("target_patients");
+
+        Dataset<Row> unmatchedFromJoinDf = session.sql("SELECT t.* FROM target_patients t LEFT ANTI JOIN source_patients s ON s.SiteCode <=> t.SiteCode AND" +
+                " s.PatientPK <=> t.PatientPK AND s.Id <=> t.Id");
+
+        long unmatchedVisitCount = unmatchedFromJoinDf.count();
+        logger.info("Unmatched count after target join is: " + unmatchedVisitCount);
+        unmatchedFromJoinDf.createOrReplaceTempView("final_unmatched");
+
+        Dataset<Row> unmatchedMergeDf1 = session.sql("SELECT Id,PatientID,PatientPK,SiteCode,FacilityName,Gender," +
+                "DOB,RegistrationDate,RegistrationAtCCC,RegistrationAtPMTCT,RegistrationAtTBClinic,PatientSource,Region," +
+                "District,Village,ContactRelation,LastVisit,MaritalStatus,EducationLevel,DateConfirmedHIVPositive," +
+                "PreviousARTExposure,PreviousARTStartDate,Emr,Project,Orphan,Inschool,PatientType,PopulationType," +
+                "KeyPopulationType,PatientResidentCounty,PatientResidentSubCounty,PatientResidentLocation," +
+                "PatientResidentSubLocation,PatientResidentWard,PatientResidentVillage,TransferInDate,Occupation,NUPI,CKV" +
+                " FROM final_unmatched");
+
+        Dataset<Row> sourceMergeDf2 = session.sql("SELECT Id,PatientID,PatientPK,SiteCode,FacilityName,Gender," +
+                "DOB,RegistrationDate,RegistrationAtCCC,RegistrationAtPMTCT,RegistrationAtTBClinic,PatientSource,Region," +
+                "District,Village,ContactRelation,LastVisit,MaritalStatus,EducationLevel,DateConfirmedHIVPositive," +
+                "PreviousARTExposure,PreviousARTStartDate,Emr,Project,Orphan,Inschool,PatientType,PopulationType," +
+                "KeyPopulationType,PatientResidentCounty,PatientResidentSubCounty,PatientResidentLocation," +
+                "PatientResidentSubLocation,PatientResidentWard,PatientResidentVillage,TransferInDate,Occupation,NUPI,CKV" +
+                " FROM source_patients");
+
+        sourceMergeDf2.printSchema();
+        unmatchedMergeDf1.printSchema();
 
         // Will "update" all rows matched, insert new rows and maintain any unmatched rows
-        Dataset<Row> finalMergeDf = sourceDf.unionAll(unmatchedDf);
+        Dataset<Row> finalMergeDf = sourceMergeDf2.union(unmatchedMergeDf1);
+
+        long mergedFinalCount = finalMergeDf.count();
+        logger.info("Merged final count: " + mergedFinalCount);
 
         logger.info("Writing final dataframe to target table");
         // Write to target table
         finalMergeDf
-                .repartition(10)
                 .write()
                 .format("jdbc")
                 .option("url", rtConfig.get("spark.sink.url"))
