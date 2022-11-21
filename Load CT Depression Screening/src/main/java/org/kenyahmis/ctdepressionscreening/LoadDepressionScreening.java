ecccea514e3ab67dff403efe309494adcc3b12b8
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-11-21 19:20:43 +0300

    Add depression screening job

---
+++ Load CT Depression Screening/src/main/java/org/kenyahmis/ctdepressionscreening/LoadDepressionScreening.java
@@ -0,0 +1,103 @@
+package org.kenyahmis.ctdepressionscreening;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.*;
+import org.apache.spark.storage.StorageLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+public class LoadDepressionScreening {
+    private static final Logger logger = LoggerFactory.getLogger(LoadDepressionScreening.class);
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("Load Depression Screening");
+        SparkSession session = SparkSession.builder()
+                .config(conf)
+                .getOrCreate();
+        RuntimeConfig rtConfig = session.conf();
+
+        final String sourceQueryFileName = "LoadDepressionScreening.sql";
+        String sourceQuery;
+        InputStream inputStream = LoadDepressionScreening.class.getClassLoader().getResourceAsStream(sourceQueryFileName);
+        if (inputStream == null) {
+            logger.error(sourceQueryFileName + " not found");
+            return;
+        }
+        try {
+            sourceQuery = IOUtils.toString(inputStream, Charset.defaultCharset());
+        } catch (IOException e) {
+            logger.error("Failed to load Depression screening query from file", e);
+            return;
+        }
+
+        logger.info("Loading source depression screening");
+        Dataset<Row> sourceDf = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.source.url"))
+                .option("driver", rtConfig.get("spark.source.driver"))
+                .option("user", rtConfig.get("spark.source.user"))
+                .option("password", rtConfig.get("spark.source.password"))
+                .option("dbtable", "(" + sourceQuery + ") pv")
+                .option("numpartitions", rtConfig.get("spark.source.numpartitions"))
+                .load();
+
+        sourceDf.persist(StorageLevel.DISK_ONLY());
+
+        logger.info("Loading target Depression screening");
+        Dataset<Row> targetDf = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.sink.url"))
+                .option("driver", rtConfig.get("spark.sink.driver"))
+                .option("user", rtConfig.get("spark.sink.user"))
+                .option("password", rtConfig.get("spark.sink.password"))
+                .option("dbtable", rtConfig.get("spark.sink.dbtable"))
+                .load();
+
+        targetDf.persist(StorageLevel.DISK_ONLY());
+        sourceDf.createOrReplaceTempView("source_depression_screening");
+        targetDf.createOrReplaceTempView("target_depression_screening");
+
+        Dataset<Row> unmatchedFromJoinDf = session.sql("SELECT t.* FROM target_depression_screening t LEFT ANTI JOIN source_depression_screening s ON s.SiteCode <=> t.SiteCode AND" +
+                " s.PatientPK <=> t.PatientPK AND s.DepressionScreeningUnique_ID <=> t.DepressionScreeningUnique_ID AND s.VisitID <=> t.VisitID");
+
+        long unmatchedVisitCount = unmatchedFromJoinDf.count();
+        logger.info("Unmatched count after target join is: " + unmatchedVisitCount);
+        unmatchedFromJoinDf.createOrReplaceTempView("final_unmatched");
+
+        Dataset<Row> unmatchedMergeDf1 = session.sql("SELECT PatientID,PatientPK,SiteCode,FacilityName,VisitID," +
+                "VisitDate,Emr,Project,PHQ9_1,PHQ9_2,PHQ9_3,PHQ9_4,PHQ9_5,PHQ9_6,PHQ9_7,PHQ9_8,PHQ9_9,PHQ_9_rating," +
+                "DepressionAssesmentScore,DateImported,CKV,PatientUnique_ID,DepressionScreeningUnique_ID" +
+                " FROM final_unmatched");
+
+        Dataset<Row> sourceMergeDf2 = session.sql("SELECT PatientID,PatientPK,SiteCode,FacilityName,VisitID," +
+                "VisitDate,Emr,Project,PHQ9_1,PHQ9_2,PHQ9_3,PHQ9_4,PHQ9_5,PHQ9_6,PHQ9_7,PHQ9_8,PHQ9_9,PHQ_9_rating," +
+                "DepressionAssesmentScore,DateImported,CKV,PatientUnique_ID,DepressionScreeningUnique_ID" +
+                " FROM source_depression_screening");
+
+        sourceMergeDf2.printSchema();
+        unmatchedMergeDf1.printSchema();
+
+        Dataset<Row> dfMergeFinal = unmatchedMergeDf1.union(sourceMergeDf2);
+
+        long mergedFinalCount = dfMergeFinal.count();
+        logger.info("Merged final count: " + mergedFinalCount);
+
+        // Write to target table
+        dfMergeFinal
+                .write()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.sink.url"))
+                .option("driver", rtConfig.get("spark.sink.driver"))
+                .option("user", rtConfig.get("spark.sink.user"))
+                .option("password", rtConfig.get("spark.sink.password"))
+                .option("dbtable", rtConfig.get("spark.sink.dbtable"))
+                .option("truncate", "true")
+                .mode(SaveMode.Overwrite)
+                .save();
+    }
+}
