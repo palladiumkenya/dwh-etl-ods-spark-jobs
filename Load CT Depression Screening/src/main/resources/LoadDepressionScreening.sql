31f1d8ac9ab8c258efe3a6cd8e17a5da5d046a37
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-11-21 19:20:43 +0300

    Add depression screening job

---
+++ Load CT Depression Screening/src/main/resources/LoadDepressionScreening.sql
@@ -0,0 +1,22 @@
+SELECT
+    P.[PatientCccNumber] AS PatientID,P.[PatientPID] AS PatientPK,F.Code AS SiteCode,F.Name AS FacilityName,
+    DS.[VisitId] AS VisitID,DS.[VisitDate] AS VisitDate,P.[Emr],
+    CASE
+        P.[Project]
+        WHEN 'I-TECH' THEN 'Kenya HMIS II'
+        WHEN 'HMIS' THEN 'Kenya HMIS II'
+        ELSE P.[Project]
+        END AS Project,
+    DS.[PHQ9_1],DS.[PHQ9_2],DS.[PHQ9_3],DS.[PHQ9_4],DS.[PHQ9_5],DS.[PHQ9_6],DS.[PHQ9_7],
+    DS.[PHQ9_8],DS.[PHQ9_9],DS.[PHQ_9_rating],DS.[DepressionAssesmentScore],
+    GETDATE() AS DateImported,
+    -- DS.[Voided],
+    -- DS.[Processed],
+    -- DS.[Created],
+    LTRIM(RTRIM(STR(F.Code))) + '-' + LTRIM(RTRIM(P.[PatientCccNumber])) + '-' + LTRIM(RTRIM(STR(P.[PatientPID]))) AS CKV
+        ,P.ID as PatientUnique_ID
+        ,DS.ID as DepressionScreeningUnique_ID
+FROM [DWAPICentral].[dbo].[PatientExtract](NoLock) P
+    INNER JOIN [DWAPICentral].[dbo].[DepressionScreeningExtract](NoLock) DS ON DS.[PatientId] = P.ID AND DS.Voided = 0
+    INNER JOIN [DWAPICentral].[dbo].[Facility](NoLock) F ON P.[FacilityId] = F.Id AND F.Voided = 0
+WHERE P.gender != 'Unknown'
