372fe6b70539ba8c0684249c3c48dce8607b416f
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-12-15 07:00:33 +0300

    Code format

--- Load CT Patient Visits/src/main/java/org/kenyahmis/loadctpatientvisits/LoadPatientVisits.java
+++ Load CT Patient Visits/src/main/java/org/kenyahmis/loadctpatientvisits/LoadPatientVisits.java
@@ -7,7 +7,6 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.storage.StorageLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
