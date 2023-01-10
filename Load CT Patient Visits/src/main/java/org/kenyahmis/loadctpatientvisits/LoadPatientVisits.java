842f2f7d26d7d5831bb5aee0da4500f57002562a
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2023-01-10 20:22:57 +0300

    Add update rows and append new data logic

--- Load CT Patient Visits/src/main/java/org/kenyahmis/loadctpatientvisits/LoadPatientVisits.java
+++ Load CT Patient Visits/src/main/java/org/kenyahmis/loadctpatientvisits/LoadPatientVisits.java
@@ -7,10 +7,11 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.storage.StorageLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.sql.Date;
+import java.sql.*;
 import java.time.LocalDate;
 
 import static org.apache.spark.sql.functions.*;
@@ -127,38 +128,30 @@ public class LoadPatientVisits {
         targetDf.createOrReplaceTempView("target_visits");
         sourceDf.createOrReplaceTempView("source_visits");
 
-        Dataset<Row> unmatchedFromJoinDf = session.sql("SELECT t.* FROM target_visits t LEFT ANTI JOIN " +
-                "source_visits s ON s.PatientPK <=> t.PatientPK AND s.SiteCode <=> t.SiteCode AND s.VisitID <=> t.VisitID ");
+        // Get new records
+        Dataset<Row> newRecordsJoinDf = session.sql("SELECT s.* FROM source_visits s LEFT ANTI JOIN " +
+                "target_visits t ON s.PatientPK <=> t.PatientPK AND s.SiteCode <=> t.SiteCode AND s.VisitID <=> t.VisitID ");
 
-        long unmatchedVisitCount = unmatchedFromJoinDf.count();
-        logger.info("Unmatched count after target join is: " + unmatchedVisitCount);
-        unmatchedFromJoinDf.createOrReplaceTempView("final_unmatched");
+        long newVisitCount = newRecordsJoinDf.count();
+        logger.info("New record count is: " + newVisitCount);
+        newRecordsJoinDf.createOrReplaceTempView("new_records");
 
-        Dataset<Row> unMatchedMergeDf1 = session.sql("select PatientID,FacilityName,SiteCode,PatientPK,VisitID," +
+        // Get matched records
+        if (rtConfig.contains("spark.ods.update") && rtConfig.get("spark.ods.update").equals("yes")) {
+            LoadPatientVisits loadPatientVisits = new LoadPatientVisits();
+            loadPatientVisits.updateMatchedRecords(session, rtConfig);
+        }
+
+        newRecordsJoinDf = session.sql("select PatientID,FacilityName,SiteCode,PatientPK,VisitID," +
                 "VisitDate,SERVICE,VisitType,WHOStage,WABStage,Pregnant,LMP,EDD,Height,Weight,BP,OI,OIDate,Adherence," +
                 "AdherenceCategory,FamilyPlanningMethod,PwP,GestationAge,NextAppointmentDate,Emr,Project,CKV," +
                 "DifferentiatedCare,StabilityAssessment,KeyPopulationType,PopulationType,VisitBy,Temp,PulseRate," +
                 "RespiratoryRate,OxygenSaturation,Muac,NutritionalStatus,EverHadMenses,Breastfeeding,Menopausal," +
                 "NoFPReason,ProphylaxisUsed,CTXAdherence,CurrentRegimen,HCWConcern,TCAReason,ClinicalNotes," +
-                "PatientUnique_ID,PatientVisitUnique_ID from final_unmatched");
-
-        Dataset<Row> sourceMergeDf2 = session.sql("select PatientID,FacilityName,SiteCode,PatientPK,VisitID," +
-                "VisitDate,SERVICE,VisitType,WHOStage,WABStage,Pregnant,LMP,EDD,Height,Weight,BP,OI,OIDate,Adherence," +
-                "AdherenceCategory,FamilyPlanningMethod,PwP,GestationAge,NextAppointmentDate,Emr,Project,CKV," +
-                "DifferentiatedCare,StabilityAssessment,KeyPopulationType,PopulationType,VisitBy,Temp,PulseRate," +
-                "RespiratoryRate,OxygenSaturation,Muac,NutritionalStatus,EverHadMenses,Breastfeeding,Menopausal," +
-                "NoFPReason,ProphylaxisUsed,CTXAdherence,CurrentRegimen,HCWConcern,TCAReason,ClinicalNotes," +
-                "PatientUnique_ID,PatientVisitUnique_ID from source_visits");
-
-        // Union all records together
-        // TODO remove duplicates in final dataframe
-        Dataset<Row> dfMergeFinal = unMatchedMergeDf1.union(sourceMergeDf2);
-        // Write to target table
-        long mergedFinalCount = dfMergeFinal.count();
-        logger.info("Merged final count: " + mergedFinalCount);
+                "PatientUnique_ID,PatientVisitUnique_ID from new_records");
 
         // TODO test out removeDuplicates() before Nov launch
-        dfMergeFinal
+        newRecordsJoinDf
                 .repartition(Integer.parseInt(rtConfig.get("spark.source.numpartitions")))
                 .write()
                 .format("jdbc")
@@ -167,8 +160,156 @@ public class LoadPatientVisits {
                 .option("user", rtConfig.get("spark.sink.user"))
                 .option("password", rtConfig.get("spark.sink.password"))
                 .option("dbtable", rtConfig.get("spark.sink.dbtable"))
-                .option("truncate", "true")
-                .mode(SaveMode.Overwrite)
+                .mode(SaveMode.Append)
                 .save();
+    }
+
+    private void updateMatchedRecords(SparkSession session, RuntimeConfig rtConfig) {
+        Dataset<Row> matchedRecordsDf = session.sql("SELECT s.* FROM source_visits s INNER JOIN " +
+                "target_visits t ON s.PatientPK <=> t.PatientPK AND s.SiteCode <=> t.SiteCode AND s.VisitID <=> t.VisitID");
+
+        matchedRecordsDf.printSchema();
+        matchedRecordsDf.createOrReplaceTempView("matched_records");
+        matchedRecordsDf = session.sql("select PatientID,FacilityName,SiteCode,PatientPK,VisitID," +
+                "VisitDate,SERVICE,VisitType,WHOStage,WABStage,Pregnant,LMP,EDD,Height,Weight,BP,OI,OIDate,Adherence," +
+                "AdherenceCategory,FamilyPlanningMethod,PwP,GestationAge,NextAppointmentDate,Emr,Project,CKV," +
+                "DifferentiatedCare,StabilityAssessment,KeyPopulationType,PopulationType,VisitBy,Temp,PulseRate," +
+                "RespiratoryRate,OxygenSaturation,Muac,NutritionalStatus,EverHadMenses,Breastfeeding,Menopausal," +
+                "NoFPReason,ProphylaxisUsed,CTXAdherence,CurrentRegimen,HCWConcern,TCAReason,ClinicalNotes," +
+                "PatientUnique_ID,PatientVisitUnique_ID from matched_records");
+        String dbURL = rtConfig.get("spark.sink.url");
+        String user = rtConfig.get("spark.sink.user");
+        String pass = rtConfig.get("spark.sink.password");
+        matchedRecordsDf
+                .repartition(Integer.parseInt(rtConfig.get("spark.source.numpartitions")))
+                .javaRDD().foreachPartition(part -> {
+                    final Connection conn = DriverManager.getConnection(dbURL, user, pass);
+                    try {
+                        while (part.hasNext()) {
+                            Row row = part.next();
+                            String query = "UPDATE CT_PatientVisits set " +
+                                    "SERVICE = ?,VisitType = ?,WHOStage = ?,WABStage = ?,Pregnant = ?,LMP = ?,EDD = ?,Height =?," +
+                                    "Weight = ?,BP = ?,OI = ?,OIDate = ?,Adherence = ?,AdherenceCategory = ?, FamilyPlanningMethod = ?," +
+                                    "PwP = ?,GestationAge = ?,NextAppointmentDate = ?,Emr = ?,Project = ?,CKV = ?," +
+                                    "DifferentiatedCare = ?,StabilityAssessment = ?,KeyPopulationType = ?,PopulationType = ?," +
+                                    "VisitBy = ?,Temp = ?,PulseRate = ?,RespiratoryRate = ?,OxygenSaturation = ?,Muac = ?," +
+                                    "NutritionalStatus = ?,EverHadMenses = ?,Breastfeeding = ?,Menopausal = ?,NoFPReason = ?," +
+                                    "ProphylaxisUsed = ?,CTXAdherence = ?,CurrentRegimen = ?,HCWConcern = ?, TCAReason = ?," +
+                                    "ClinicalNotes = ?,PatientUnique_ID = ?,PatientVisitUnique_ID = ? " +
+                                    "WHERE PatientPK = ? AND SiteCode = ? AND VisitID = ?";
+                            PreparedStatement preparedStatement = conn.prepareStatement(query);
+                            preparedStatement.setString(1, row.getAs("SERVICE"));
+                            preparedStatement.setString(2, row.getAs("VisitType"));
+                            if (row.getAs("WHOStage") != null) {
+                                preparedStatement.setString(3, row.getAs("WHOStage").toString());
+                            } else {
+                                preparedStatement.setNull(3, Types.NULL);
+                            }
+                            preparedStatement.setString(4, row.getAs("WABStage"));
+                            preparedStatement.setString(5, row.getAs("Pregnant"));
+                            if (row.getAs("LMP") != null) {
+                                preparedStatement.setString(6, row.getAs("LMP").toString());
+                            } else {
+                                preparedStatement.setNull(6, Types.NULL);
+                            }
+                            if (row.getAs("EDD") != null) {
+                                preparedStatement.setString(7, row.getAs("EDD").toString());
+                            } else {
+                                preparedStatement.setNull(7, Types.NULL);
+                            }
+
+                            preparedStatement.setString(8, row.getAs("Height"));
+                            preparedStatement.setString(9, row.getAs("Weight"));
+                            preparedStatement.setString(10, row.getAs("BP"));
+                            preparedStatement.setString(11, row.getAs("OI"));
+                            if (row.getAs("OIDate") != null) {
+                                preparedStatement.setDate(12, new Date(((Timestamp) row.getAs("OIDate")).getTime()));
+                            } else {
+                                preparedStatement.setNull(12, Types.NULL);
+                            }
+
+                            preparedStatement.setString(13, row.getAs("Adherence"));
+                            preparedStatement.setString(14, row.getAs("AdherenceCategory"));
+                            preparedStatement.setString(15, row.getAs("FamilyPlanningMethod"));
+                            preparedStatement.setString(16, row.getAs("PwP"));
+                            if (row.getAs("GestationAge") != null) {
+                                preparedStatement.setString(17, row.getAs("GestationAge").toString());
+                            } else {
+                                preparedStatement.setNull(17, Types.NULL);
+                            }
+
+                            if (row.getAs("NextAppointmentDate") != null) {
+                                preparedStatement.setDate(18, new Date(((Timestamp) row.getAs("NextAppointmentDate")).getTime()));
+                            } else {
+                                preparedStatement.setNull(18, Types.NULL);
+                            }
+                            preparedStatement.setString(19, row.getAs("Emr"));
+                            preparedStatement.setString(20, row.getAs("Project"));
+                            preparedStatement.setString(21, row.getAs("CKV"));
+                            preparedStatement.setString(22, row.getAs("DifferentiatedCare"));
+                            preparedStatement.setString(23, row.getAs("StabilityAssessment"));
+                            preparedStatement.setString(24, row.getAs("KeyPopulationType"));
+                            preparedStatement.setString(25, row.getAs("PopulationType"));
+                            preparedStatement.setString(26, row.getAs("VisitBy"));
+                            if (row.getAs("Temp") != null) {
+                                preparedStatement.setBigDecimal(27, row.getAs("Temp"));
+                            } else {
+                                preparedStatement.setNull(27, Types.NULL);
+                            }
+
+                            if (row.getAs("PulseRate") != null) {
+                                preparedStatement.setInt(28, row.getAs("PulseRate"));
+                            } else {
+                                preparedStatement.setNull(28, Types.NULL);
+                            }
+
+                            if (row.getAs("RespiratoryRate") != null) {
+                                preparedStatement.setInt(29, row.getAs("RespiratoryRate"));
+                            } else {
+                                preparedStatement.setNull(29, Types.NULL);
+                            }
+                            if (row.getAs("OxygenSaturation") != null) {
+                                preparedStatement.setBigDecimal(30, row.getAs("OxygenSaturation"));
+                            } else {
+                                preparedStatement.setNull(30, Types.NULL);
+                            }
+                            if (row.getAs("Muac") != null) {
+                                preparedStatement.setInt(31, row.getAs("Muac"));
+                            } else {
+                                preparedStatement.setNull(31, Types.NULL);
+                            }
+                            preparedStatement.setString(32, row.getAs("NutritionalStatus"));
+                            preparedStatement.setString(33, row.getAs("EverHadMenses"));
+                            preparedStatement.setString(34, row.getAs("Breastfeeding"));
+                            preparedStatement.setString(35, row.getAs("Menopausal"));
+                            preparedStatement.setString(36, row.getAs("NoFPReason"));
+                            preparedStatement.setString(37, row.getAs("ProphylaxisUsed"));
+                            preparedStatement.setString(38, row.getAs("CTXAdherence"));
+                            preparedStatement.setString(39, row.getAs("CurrentRegimen"));
+                            preparedStatement.setString(40, row.getAs("HCWConcern"));
+                            preparedStatement.setString(41, row.getAs("TCAReason"));
+                            preparedStatement.setString(42, row.getAs("ClinicalNotes"));
+                            preparedStatement.setString(43, row.getAs("PatientUnique_ID"));
+                            preparedStatement.setString(44, row.getAs("PatientVisitUnique_ID"));
+                            preparedStatement.setString(45, row.getAs("PatientPK").toString());
+                            preparedStatement.setString(46, row.getAs("SiteCode").toString());
+                            preparedStatement.setString(47, row.getAs("VisitID").toString());
+                            int rows = preparedStatement.executeUpdate();
+                            logger.info(rows + " affected in visit update");
+                        }
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    } finally {
+                        try {
+                            if (conn != null && !conn.isClosed()) {
+                                conn.close();
+                            }
+                        } catch (SQLException ex) {
+                            logger.error("Failed to close sql connection");
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+        );
     }
 }
